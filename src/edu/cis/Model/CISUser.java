a1d4481b1334dc7aa6a684d2fdd628388f3359be
ashleyw2024 <ashleyw2024@student.cis.edu.hk>
2022-12-14 12:58:18 +0800

    implemented getter and setter method in CISUser

--- src/edu/cis/Model/CISUser.java
+++ src/edu/cis/Model/CISUser.java
@@ -1,5 +1,52 @@
 package edu.cis.Model;
 
+import java.util.ArrayList;
+
 public class CISUser {
 
+    private String userID;
+    private String name;
+    private String yearLevel;
+    private ArrayList<Order> orders;
+    private double money;
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getYearLevel() {
+        return yearLevel;
+    }
+
+    public void setYearLevel(String yearLevel) {
+        this.yearLevel = yearLevel;
+    }
+
+    public ArrayList<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(ArrayList<Order> orders) {
+        this.orders = orders;
+    }
+
+    public double getMoney() {
+        return money;
+    }
+
+    public void setMoney(double money) {
+        this.money = money;
+    }
+
+    public String getUserID(){
+        return this.userID;
+    }
 }
